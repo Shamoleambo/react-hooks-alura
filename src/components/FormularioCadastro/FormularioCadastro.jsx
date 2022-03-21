755915a4f28f06b41e79a3af766ce1454a203752
Tiago Gomes <tidgomes@gmail.com>
2022-03-21 11:11:46 -0300

    creation of formularioAtual function to control steps

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
+import { Typography } from "@material-ui/core";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
-  return (
-    <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-      <DadosUsuario />
-      <DadosEntrega />
-    </>
-  );
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  return <>{formularioAtual(etapaAtual)}</>;
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />;
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />;
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>;
+    }
+  }
 }
 
 export default FormularioCadastro;
